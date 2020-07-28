87c2b904d3fb20071fc0e349ccfb785e8aa4109d
Austin Chen <austin.yhc@gmail.com>
2020-07-28 15:23:34 +0800

    Correct the Serial Reading Logics
    Missing response. In practice, we don't separate write and read into two different
    connection session.

--- sim7000.py
+++ sim7000.py
@@ -6,8 +6,8 @@ from datetime import datetime
 
 CMD_LINEBREAK = b'\r\n'
 
-PORT = "/dev/ttyUSB0"
-BAUD = 9600
+PORT = "/dev/ttyUSB2"
+BAUD = 115200
 
 # Mosquitto.org Settings
 MQTT_URL="test.mosquitto.org"
@@ -17,14 +17,16 @@ CERT_NAME = "mosquitto.crt"
 KEY_NAME = "mosquitto.key"
 
 def send(data):
-    with serial.Serial(PORT, BAUD, timeout=1) as ser:
+    with serial.Serial(PORT, BAUD, xonxoff=False,
+            rtscts=True, dsrdtr=True, timeout=6) as ser:
         ser.write(data)
 
-def send_cmd(cmd):
-    send(cmd.encode('utf-8') + CMD_LINEBREAK)
+def send_and_watch(cmd, timeout=10, success=None, failure=None, echo_cmd=None):
+    with serial.Serial(PORT, BAUD, xonxoff=True,
+            rtscts=True, dsrdtr=True, timeout=timeout) as ser:
 
-def watch(timeout=10, success=None, failure=None, echo_cmd=None):
-    with serial.Serial(PORT, BAUD, timeout=1) as ser:
+        ser.write(cmd.encode('utf-8') + CMD_LINEBREAK)
+
         t_start = time.time()
         reply = list()
         while True:
@@ -32,7 +34,7 @@ def watch(timeout=10, success=None, failure=None, echo_cmd=None):
                 line = ser.readline()
                 echo = False
                 if echo_cmd:
-                    echo = line.decode('utf-8').strip().endswith(echo_cmd)
+                    echo = line.decode('utf-8').strip().startswith(echo_cmd)
                 if line != CMD_LINEBREAK and not echo:
                     line = line.decode('utf-8').strip()
                     reply.append('\t' + line)
@@ -44,11 +46,10 @@ def watch(timeout=10, success=None, failure=None, echo_cmd=None):
                 return ("Timeout", reply, time.time()-t_start)
             time.sleep(0.02)
 
-def AT(cmd="", timeout=10, success="OK", failure="+CME ERROR"):
+def AT(cmd="", timeout=5, success="OK", failure="+CME ERROR"):
     cmd = 'AT' + cmd
     print("----------- ", cmd, " -----------")
-    send_cmd(cmd)
-    reply = watch(echo_cmd=cmd, timeout=timeout, success=success, failure=failure)
+    reply = send_and_watch(cmd, echo_cmd=cmd, timeout=timeout, success=success, failure=failure)
     print("{0} ({1:.2f}secs):".format(reply[0], reply[2]))
     print(*reply[1], sep='\n')
     print('')
